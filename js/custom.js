3dc9eba6be495b75b8b35ab16909852cc1a0b170
Timofey <tshevchenko@ro.ru>
2018-03-18 09:47:42 +0200

    last edit

--- js/custom.js
+++ js/custom.js
@@ -14,32 +14,45 @@ Vue.component('list-item', {
 		},
 	},
 	template: `
-		<div class="music-item">
-			<div v-on:click="opened" class="music-item_header">
-				<div class="music-item_image"><img :src="item.сollectionImage" :alt="item.collectionName" class="d-none d-lg-inline img-fluid"></div>
-				<div class="music-item_artist">{{item.artist}}</div>
-				<div class="music-item_track">{{item.track}}</div>
-				<div class="music-item_collection">{{item.collectionName}}</div>
-				<div class="music-item_genre">{{item.ganre}}</div>
-				<div class="music-item_btn">
-					<i v-if="item.open" class="fas fa-minus"></i>
-					<i v-else class="fas fa-plus"></i>
-				</div>
+	<!-- START ITEM -->
+	<div class="music-item">
+		<div v-on:click="opened" class="music-item_header">
+			<div class="music-item_image">
+				<img 
+					:src="item.сollectionImage" 
+					:alt="item.collectionName"
+				 	class="d-none d-lg-inline img-fluid"
+				>
 			</div>
-			<div v-if="item.open" class="music-item_body">
-				<div class="music-item_image"><img :src="item.сollectionImage" :alt="item.collectionName" class="d-inline d-lg-none img-fluid"></div>
-				<div class="music-item_big-section">
-					<h3> {{item.artist}} - {{item.track}} <i class="fas fa-music"></i></h3>
-					<p><b>Collection:</b> {{item.collectionName}} </p>
-					<p><b>Track Count:</b> {{item.trackCount}} </p>
-					<p><b>Price:</b> {{item.price}}$</p>
-				</div>
-				<div class="music-item_big-section">
-					<p><b>Track Duration:</b> {{item.trackDuration | time }} min</p>
-					<p><b>Track Price:</b> {{item.trackPrice}}$</p>
-				</div>
+			<div class="music-item_artist">{{item.artist}}</div>
+			<div class="music-item_track">{{item.track}}</div>
+			<div class="music-item_collection">{{item.collectionName}}</div>
+			<div class="music-item_genre">{{item.ganre}}</div>
+			<div class="music-item_btn">
+				<i v-if="item.open" class="fas fa-minus"></i>
+				<i v-else class="fas fa-plus"></i>
 			</div>
 		</div>
+		<div v-if="item.open" class="music-item_body">
+			<div class="music-item_image">
+				<img 
+					:src="item.сollectionImage" 
+					:alt="item.collectionName"
+					class="d-inline d-lg-none img-fluid"
+				>
+			</div>
+			<div class="music-item_big-section">
+				<h3> {{item.artist}} - {{item.track}} <i class="fas fa-music"></i></h3>
+				<p><b>Collection:</b> {{item.collectionName}} </p>
+				<p><b>Track Count:</b> {{item.trackCount}} </p>
+				<p><b>Price:</b> {{item.price}}$</p>
+			</div>
+			<div class="music-item_big-section">
+				<p><b>Track Duration:</b> {{item.trackDuration | time }} min</p>
+				<p><b>Track Price:</b> {{item.trackPrice}}$</p>
+			</div>
+		</div>
+	</div><!-- END ITEM -->
 	`,
 });
 
@@ -48,13 +61,9 @@ Vue.filter('time', function(value){
 	let date = new Date(value);
 	let min = date.getMinutes().toString();
 	let sec = date.getSeconds().toString();
-	// ADDS ZEROS TO A SINGLE NUMBER
-	if (min.length === 1) { 
-		min = '0'+min;
-	}
-	if (sec.length === 1) {
-		sec = '0'+sec;
-	}
+	// ADD ZERO TO A SINGLE NUMBER
+	if( min.length === 1 ) { min = '0' + min; } 
+	if( sec.length === 1 ) { sec = '0'+sec; }
 	return min + ' : ' + sec;
 });
 
@@ -62,12 +71,12 @@ Vue.filter('time', function(value){
 new Vue ({
 	el: '#music',
 	data: {
-		searchString: '',
-		list: [],
-		timeout: '',
+		searchString: '', // SEARCH INPUT FIELD
+		list: [], // LIST OF SONGS
+		timeout: '', // PREVENT UNNECESSARY REQUESTS
 	},
 	watch: {
-		// OBSERVE THE SEARCH FORM
+		// WATCHING THE SEARCH FORM
 		searchString: function (newQuestion, oldQuestion) {
 			clearTimeout(this.timeout);
 			this.timeout = setTimeout( () => { 
@@ -83,7 +92,7 @@ new Vue ({
 			.then( response => response.json() )
 			.then( data => {
 				this.list = [];
-				// SELECT THE REQUIRED DATA
+				// DATA SELECTION
 				this.list.push(data.results.map(item => {
 					return {
 						сollectionImage: item.artworkUrl100,
